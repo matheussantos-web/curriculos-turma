f865f78974b0df28908cd0a3374464082cf2e26f
teaga.dev <matheus281197@gmail.com>
2026-09-03 10:17:38 -0300

    criação da integração com supabase

---
+++ assets/js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://eempgprspimlhzlofvjx.supabase.co";
+const SUPABASE_KEY = "sb_publishable_H8eepB7xyJndr_TdsvuiVw_pJbsPC5k";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
